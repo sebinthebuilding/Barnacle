8fee5b78df1c2d8ec91a1755d03929d9e03585aa
sebinthebuilding <sebike@sebby>
2023-09-11 11:20:47 +0200

    modified?

--- barnacle-so/src/math.rs
+++ barnacle-so/src/math.rs
@@ -4,15 +4,15 @@ use jup_ag::price;
 use solana_sdk::pubkey::Pubkey;
 
 pub async fn latest_price_delta() -> f64 {
-    let sol_pubkey = Pubkey::from_str("So11111111111111111111111111111111111111112").unwrap();
-    let usdc_pubkey = Pubkey::from_str("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v").unwrap();
+    let sol_pubkey: Pubkey = Pubkey::from_str("So11111111111111111111111111111111111111112").unwrap();
+    let usdc_pubkey: Pubkey = Pubkey::from_str("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v").unwrap();
     
     loop {
-        let price1 = price(sol_pubkey, usdc_pubkey, 1.0).await.unwrap().data.price;
-        let price2 = price(sol_pubkey, usdc_pubkey, 1.0).await.unwrap().data.price;
+        let price1: f64 = price(sol_pubkey, usdc_pubkey, 1.0).await.unwrap().data.price;
+        let price2: f64 = price(sol_pubkey, usdc_pubkey, 1.0).await.unwrap().data.price;
 
         if price1 != price2 {
-            println!("{}", (price1 - price2).abs());
+            println!("{}\n{}", price1, price2);
             return (price1 - price2).abs()
         }
     }
