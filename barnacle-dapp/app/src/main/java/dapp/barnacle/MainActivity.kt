468a7a34b2a23e4715d6ef875900ae35b0c21316
sebinthebuilding <sebbysandor@gmail.com>
2023-09-18 19:31:52 +0200

    need to open page in new activity

--- barnacle-dapp/app/src/main/java/dapp/barnacle/MainActivity.kt
+++ barnacle-dapp/app/src/main/java/dapp/barnacle/MainActivity.kt
@@ -1,11 +1,18 @@
 package dapp.barnacle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val holdingsButton: Button = findViewById(R.id.holdings_button);
+        holdingsButton.setOnClickListener {
+            Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show()
+        }
     }
 }
